e004288ed334ad4eb740781bbeb653098141fd2b
Aaron <aaronp.jones7@gmail.com>
2020-10-02 10:10:30 -0700

    cleaned up comments and old tests

--- meal_plan.py
+++ meal_plan.py
@@ -1,17 +1,16 @@
 import random
 from collections import Counter
 # Functionality to add:
-# Don't use random or find a unqiue Function to stop meal repeats through out the week
 # Email me the list every Sunday
+# turn ingredients into an tuples (ingrd name + quantity)
 # move the data/objects to a file in json format and read it in.
 # put it in a webserver(this implies database)
 
 class meal:
     def __init__(self, name, ingrd1, ingrd2, ingrd3, ingrd4, ingrd5, ingrd6, ingrd7, ingrd8):
 
-      ##Assgn var values
+      #Assgn var values
         self.name = name
-        #turn ingredients into an tuples (ingrd name + quantity)
         self.ingrd1 = ingrd1
         self.ingrd2 = ingrd2
         self.ingrd3 = ingrd3
@@ -19,7 +18,7 @@ class meal:
         self.ingrd5 = ingrd5
         self.ingrd6 = ingrd6
         self.ingrd7 = ingrd7
-        self.ingrd8 = ingrd8  #create a default value later
+        self.ingrd8 = ingrd8
 
     def ingrd_list(meal):
         print ( "Ingredients: "+ meal.ingrd1 +", "+ meal.ingrd2 +", "+ meal.ingrd3 +", "+ meal.ingrd4 +", "+ meal.ingrd5 +", "+ meal.ingrd6 +", "+ meal.ingrd7 +", "+ meal.ingrd8 + "\n")
@@ -47,19 +46,19 @@ m15 = meal("Falafel","ChickPeas", "Butter","Garlic", "Lemon", "Edmame", "Pepper"
 m16 = meal("Chicken and yam","Chicken Breast", "Yams", "Garlic", "Lemon", "Olive Oil", "Pepper", "Salt", "Spice")
 m17 = meal("Greek spanakopita and potato", "Philo (Frozen)", "Butter","Garlic", "Lemon", "Olive Oil", "Pepper", "Salt", "Spice")
 m18 = meal("Bhan Mi", "Tofu", "Buns","Garlic", "Carrots", "Dikon", "Pepper", "Salt", "Spice")
+m19 = meal("Tacos", "Chicken Breat", "Tort Wraps","Garlic", "Yams", "Colslaw", "Pepper", "Salt", "Spice")
 #####data######
 
-# Randomly Select 1 through 3, event vually random select from the len of an array??
+#Load Arrays
 meals = [m1, m2, m3, m4, m5, m6, m7, m8, m9 , m10, m11, m12, m13, m14, m15, m16, m17, m18]
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-#Print Each day and unqiueselection for each day - need to make it unique
-
+#Print Each day and unqiue selection for each day
 def meal_per_day(days): #
     meallist = []
     for day in days:
         mealselected = random.choice(meals)
-        #Loop to handle conflict and retry - rather than miss day.
+        #Loop to handle conflict and retry
         stoploop = 1
         while stoploop == 1:
             #decided if selected meal is in the array
@@ -70,23 +69,21 @@ def meal_per_day(days): #
                 print ("+++++++")
                 stoploop = 0
             else:
-                # print ("Conflict of selected in the list")
                 mealselected = random.choice(meals)
                 stoploop = 1
-                # mealselected = random.choice(meals)
+
     return meallist
 
 # groceries list
 def grocery_list(mealsselected):
-    #define list to become of all ingriendts - this needs to be a function after the logic is done
+    #define list to become of all ingriendts
     ingdrs = []
     #loop over all meals
     for meal in mealsselected:
-        #loop over all ingredients for each meal # ingdrs = meal.ingrd_array()
+        #loop over all ingredients for each meal
         for i in meal.ingrd_array():
             #add ingredients one by one to 1 list
             ingdrs.append(i)
-
         #convert to dict ingredients and count
         ingdrs_dict = dict(Counter(ingdrs))
     print ("\nGrocery List: ")
@@ -94,5 +91,4 @@ def grocery_list(mealsselected):
     for key, value in ingdrs_dict.items():
         print (f'{key:20}{value}')
 
-# meal_per_day(days)
 grocery_list(meal_per_day(days))
